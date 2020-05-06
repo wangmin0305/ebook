aedc3c1514d8f983e2af7aa478f5a567e3ac7754
wangmin <751041680@qq.com>
2020-05-06 16:26:24 +0800

    提交代码

---
+++ src/Ebook.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ebook">
+      
+  </div>
+</template>
+<script>
+import Epub from "epubjs";
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub';
+export default {
+  // 电子书的解析和渲染
+  methods: {
+    showEpub() {
+      // 生成Book
+      this.book = new Epub(DOWNLOAD_URL);
+      console.log(this.book);
+      console.log(11)
+      //生成Rendtion
+      //通过Rendtion.display渲染电子书
+    },
+  },
+  mounted () {
+     this.showEpub()
+     console.log(111)
+   }
+
+};
+</script>
+<style lang="scss" scoped>
+@import "./assets/styles/global.scss";
+</style>
+
